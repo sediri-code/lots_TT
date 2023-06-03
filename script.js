047c505711df78e536d4dd1e6176ca1467a900b8
sediri-code <63328434+sediri-code@users.noreply.github.com>
2023-06-03 18:18:40 +0200

    last-of-type

--- script.js
+++ script.js
@@ -13,44 +13,26 @@ document.getElementById("addForm").addEventListener("submit", function(e) {
   var entreprise = document.getElementById("entrepriseInput").value;
   var htva = document.getElementById("htvaInput").value;
   var classement = document.getElementById("classementInput").value;
-  var rowIndex = parseInt(this.getAttribute("data-row-index"));
 
-  if (rowIndex) {
-    // If rowIndex exists, update the existing row
-    var lotTable = document.getElementById("lotTable" + lotCount);
-    var row = lotTable.rows[rowIndex];
-    var cells = row.cells;
+  var lotTable = document.querySelector("#interface2 table:last-of-type");
 
-    cells[0].innerHTML = entreprise;
-    cells[1].innerHTML = htva;
-    cells[2].innerHTML = classement;
-  } else {
-    // If rowIndex doesn't exist, add a new row
-    var lotTable = document.getElementById("lotTable" + lotCount);
-    var newRow = lotTable.insertRow();
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-    var cell3 = newRow.insertCell(2);
-    var cell4 = newRow.insertCell(3);
-    var cell5 = newRow.insertCell(4);
+  var newRow = lotTable.insertRow();
+  var cell1 = newRow.insertCell(0);
+  var cell2 = newRow.insertCell(1);
+  var cell3 = newRow.insertCell(2);
+  var cell4 = newRow.insertCell(3);
 
-    cell1.innerHTML = entreprise;
-    cell2.innerHTML = htva;
-    cell3.innerHTML = classement;
-    cell4.innerHTML = '<button class="deleteButton">Supprimer</button>';
-    cell5.innerHTML = '<button class="editButton">Modifier</button>';
+  cell1.innerHTML = entreprise;
+  cell2.innerHTML = htva;
+  cell3.innerHTML = classement;
+  cell4.innerHTML = '<button class="deleteButton">Supprimer</button>';
 
-    attachDeleteEvent();
-    attachEditEvent();
-  }
+  attachDeleteEvent();
 
   document.getElementById("entrepriseInput").value = "";
   document.getElementById("htvaInput").value = "";
   document.getElementById("classementInput").value = "";
   document.getElementById("lotForm").style.display = "none";
-
-  // Reset the form's data-row-index attribute
-  document.getElementById("addForm").removeAttribute("data-row-index");
 });
 
 function attachDeleteEvent() {
@@ -58,144 +40,47 @@ function attachDeleteEvent() {
 
   for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].addEventListener("click", function() {
-      if (confirm("Êtes-vous sûr de vouloir supprimer ce lot ?")) {
-        var row = this.parentNode.parentNode;
-        row.parentNode.removeChild(row);
-      }
-    });
-  }
-}
-
-function attachEditEvent() {
-  var editButtons = document.getElementsByClassName("editButton");
-
-  for (var i = 0; i < editButtons.length; i++) {
-    editButtons[i].addEventListener("click", function() {
       var row = this.parentNode.parentNode;
-      var cells = row.cells;
-      var rowIndex = row.rowIndex;
-
-      document.getElementById("entrepriseInput").value = cells[0].innerHTML;
-      document.getElementById("htvaInput").value = cells[1].innerHTML;
-      document.getElementById("classementInput").value = cells[2].innerHTML;
-
-      // Store the row index as a data attribute in the form
-      document.getElementById("addForm").setAttribute("data-row-index", rowIndex);
-
-      document.getElementById("lotForm").style.display = "block";
+      row.parentNode.removeChild(row);
     });
   }
 }
 
 document.getElementById("averageButton").addEventListener("click", function() {
-  var rows = document.getElementById("lotTable").rows;
-  var sum = 0;
+  var lotCount = document.getElementsByClassName("lotTable").length;
+  var totalHTVA = 0;
 
-  for (var i = 1; i < rows.length; i++) {
-    var htva = parseFloat(rows[i].cells[1].innerHTML);
+  for (var i = 1; i <= lotCount; i++) {
+    var lotTable = document.getElementById("lotTable" + i);
+    var rows = lotTable.getElementsByTagName("tr");
 
-    if (!isNaN(htva)) {
-      sum += htva;
+    for (var j = 1; j < rows.length; j++) {
+      var htva = parseFloat(rows[j].cells[1].innerHTML);
+      totalHTVA += htva;
     }
   }
 
-  var average = sum / (rows.length - 1);
-  alert("Moyenne HTVA : " + average.toFixed(2));
+  var averageHTVA = totalHTVA / (lotCount > 0 ? lotCount : 1);
+  alert("La moyenne des offres HTVA est de: " + averageHTVA.toFixed(2));
 });
-
-var lotCount = 1;
 
 document.getElementById("addLotButton").addEventListener("click", function() {
-  if (lotCount >= 10) {
-    alert("Vous avez atteint le nombre maximum de lots.");
-    return;
-  }
+  var lotCount = document.getElementsByClassName("lotTable").length;
+  var newLotNumber = lotCount + 1;
 
-  lotCount++;
+var lotNumberHeading = document.getElementById("lotNumber");
+var newLotHeading = document.createElement("h2");
+newLotHeading.id = "lotNumber" + newLotNumber;
+newLotHeading.innerText = "Lot numéro " + newLotNumber;
+lotNumberHeading.parentNode.insertBefore(newLotHeading, lotNumberHeading.nextSibling);
 
-  var interface2 = document.getElementById("interface2");
 
-  var newLot = document.createElement("div");
-  newLot.id = "lot" + lotCount;
+  var newLotTable = document.createElement("table");
+  newLotTable.id = "lotTable" + newLotNumber;
+  newLotTable.innerHTML = '<tr><th>Entreprise</th><th>Montant HTVA</th><th>Classement</th><th>Actions</th></tr>';
 
-  var lotHeading = document.createElement("h2");
-  lotHeading.textContent = "Lot numéro " + lotCount;
-  newLot.appendChild(lotHeading);
+  var addButton = document.getElementById("addButton");
+  addButton.parentNode.insertBefore(newLotTable, addButton);
 
-  var lotTable = document.createElement("table");
-  lotTable.id = "lotTable" + lotCount;
-  var tableHeader = document.createElement("tr");
-  tableHeader.innerHTML = "<th>Entreprise</th><th>Montant HTVA</th><th>Classement</th>";
-  lotTable.appendChild(tableHeader);
-  newLot.appendChild(lotTable);
-
-  var addButton = document.createElement("button");
-  addButton.id = "addButton" + lotCount;
-  addButton.textContent = "Ajouter une Entreprise";
-  newLot.appendChild(addButton);
-
-  var averageButton = document.createElement("button");
-  averageButton.id = "averageButton" + lotCount;
-  averageButton.textContent = "Calculer la moyenne des offres";
-  newLot.appendChild(averageButton);
-
-  interface2.appendChild(newLot);
-
-  attachAddEvent(lotCount);
-  attachAverageEvent(lotCount);
+  attachDeleteEvent();
 });
-
-function attachAddEvent(lotCount) {
-  var addButton = document.getElementById("addButton" + lotCount);
-  addButton.addEventListener("click", function() {
-    var lotTable = document.getElementById("lotTable1"); // Get Lot Number 1 table
-    var newRow = lotTable.insertRow();
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-    var cell3 = newRow.insertCell(2);
-    var cell4 = newRow.insertCell(3);
-    var cell5 = newRow.insertCell(4);
-
-    cell1.innerHTML = document.getElementById("entrepriseInput").value;
-    cell2.innerHTML = document.getElementById("htvaInput").value;
-    cell3.innerHTML = document.getElementById("classementInput").value;
-    cell4.innerHTML = '<button class="deleteButton">Supprimer</button>';
-    cell5.innerHTML = '<button class="editButton">Modifier</button>';
-
-    attachDeleteEvent();
-    attachEditEvent();
-
-    document.getElementById("entrepriseInput").value = "";
-    document.getElementById("htvaInput").value = "";
-    document.getElementById("classementInput").value = "";
-    document.getElementById("lotForm").style.display = "none";
-  });
-}
-
-
-
-
-
-function attachAverageEvent(lotCount) {
-  var averageButton = document.getElementById("averageButton" + lotCount);
-  averageButton.addEventListener("click", function() {
-    var lotTable = document.getElementById("lotTable" + lotCount);
-    var rows = lotTable.rows;
-    var sum = 0;
-
-    for (var i = 1; i < rows.length; i++) {
-      var htva = parseFloat(rows[i].cells[1].innerHTML);
-
-      if (!isNaN(htva)) {
-        sum += htva;
-      }
-    }
-
-    var average = sum / (rows.length - 1);
-    alert("Moyenne HTVA pour le lot " + lotCount + ": " + average.toFixed(2));
-  });
-}
-
-attachDeleteEvent();
-attachEditEvent();
-
